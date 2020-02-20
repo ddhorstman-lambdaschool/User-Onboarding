dece57bbbdf56bd3dfecec197aaa89e2e2a0d813
ddhorstman <davidzilla27@gmail.com>
2020-02-20 00:09:49 -0500

    Set up fields and values in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,40 @@
 import React from "react";
-export default function Form(props){
-    return <div />;
+import { withFormik, Form, Field } from "formik";
+import "./Form.scss";
+import * as yup from "yup";
+function UserForm(props) {
+    return (
+        <Form className="user-form">
+            <h3>New User Sign-Up</h3>
+            <label>Name:
+            <Field name="name" />
+            </label>
+            <label>Email address:
+            <Field name="email" type="email" />
+            </label>
+            <label>Password:
+            <Field name="password" type="password" />
+            </label>
+            <label>Confirm Password:
+            <Field name="confirmPassword" type="password" />
+            </label>
+            <label>
+                <Field name="acceptedTOS" type="checkbox" />
+                I have read and accepted the <a href="#">terms of service</a>.
+            </label>
+        </Form>
+    );
 }
+
+export default withFormik({
+    mapPropsToValues: () => ({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        acceptedTOS: false
+    }),
+    validationSchema: yup.object().shape({
+        name: yup.string().required()
+    })
+})(UserForm);
